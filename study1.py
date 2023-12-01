22a110a6492f1d8f749e81b14e49574ea550981c
VitorDeF <vitorfigp@gmail.com>
2023-12-01 10:18:45 -0300

    name changing and add AdvCalc class

--- study1.py
+++ study1.py
@@ -19,9 +19,25 @@ class BaseCalc:
         for i in range(1,b-1):
             a+=a
         return a
-            
+        
+class AdvCalc(BaseCalc):
+    
+    # return a binary integer number
+    def bin(a):
+        return int('{:b}'.format(a))
+    
+    # return a hexadecimal string number
+    def hex(a):
+        return str.upper('{:x}'.format(a))
+        
+    # return a octal integer number
+    def oct(a):
+        return int('{:o}'.format(a))
 
 # functions execution
 print(BaseCalc.sum(1,5))
 print(BaseCalc.rest(10,3))
 print(BaseCalc.mult(5,4))
+print(AdvCalc.bin(10))
+print(AdvCalc.oct(10))
+print(AdvCalc.hex(10))
